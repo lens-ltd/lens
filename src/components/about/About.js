e5d0fbe5c79559b85256d963a33bdb3d01e94cd8
Nishimwe Prince <princeelysee@gmail.com>
2023-06-02 14:22:53 +0200

    feature(about-page-redesign) adding the company description and portfolio
    - adds crafted messages for the company's vision and mission
    - adds products portfolio and the motivation behind them
    
    Pending:
    	-- updates the homepage with updated product names
    	-- add layout for the team and their bios

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './about.css';
 import '../../index.css';
+import { s3_bucket } from '../../helpers/constants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const About = () => {
   return (
-    <>
+    <div className="about_container">
       {/* ABOUT HERO */}
       <section className="about_hero">
         {/* ABOUT HERO HEADER */}
@@ -14,24 +16,59 @@ const About = () => {
         </div>
         {/* ABOUT HERO IMAGES */}
         <div className="about_hero_images">
-          <span className="hero_image_text">
-            <p>
-              We specialize in accounting, bookkeeping, and tax consultancy
-              activities with the exception of using our own developed simple to
-              use, both online and offline application systems.
-            </p>
-          </span>
-          <span className="hero_image_image">
-            <img
-              src="https://lens-website170952-staging.s3.amazonaws.com/images/about-hero-image.jpg"
-              alt="About hero"
-              id="about_hero_image"
-              className="about_hero_image"
-            />
-          </span>
+          <img
+            src={`${s3_bucket}/images/about-hero-image.png`}
+            id="about_hero_image"
+            alt="About hero graphic"
+          />
         </div>
       </section>
-    </>
+      {/* ABOUT US */}
+      <section className="about_us">
+        <h2>
+          At Lens, we build user-friendly tools that help individuals and
+          businesses keep track of their transactions as simply (or complex) as
+          they like.
+        </h2>
+        <button className="about_us_button">
+          {' '}
+          Get Started <FontAwesomeIcon icon="fa-solid fa-arrow-right" />{' '}
+        </button>
+      </section>
+      {/* ABOUT NICHE */}
+      <section className="about_niche">
+        <div className="about_niche_header">
+          <p>
+            Lens was created by a team of talented individuals from different
+            industries to simplify the tedious bookkeeping process as much as
+            possible.
+          </p>
+          <h2>But where did we start:</h2>
+        </div>
+        {/* ABOUT NICHE PRODUCT */}
+        <div className="about_niche_products">
+          {/* ABOUT NICHE PRODUCT - UMUHINZI */}
+          <div className="about_niche_product umuhinzi">
+            <p className="niche_product_sector">Agribusiness</p>
+            <h3 className="niche_product_description">
+              <span className='niche_product_name'>Umuhinzi App</span>
+              is helping 300+ farmers keep track of all transactions while
+              getting timely reports
+            </h3>
+            <button className="niche_product_button">Learn More</button>
+          </div>
+          {/* ABOUT NICHE PRODUCT - NYIBUTSA */}
+          <div className="about_niche_product nyibutsa">
+            <p className="niche_product_sector">Personal Finance</p>
+            <h3 className="niche_product_description">
+              <span className='niche_product_name'>Nyibutsa</span>
+              protects your integrity (and finances) with effective debt management and customized reports. 
+            </h3>
+            <button className="niche_product_button">Learn More</button>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
